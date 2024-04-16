b747c23c5a85e68307d3274963fdc815dba856dd
Rangari, Kshitij <Kshitij.Rangari@viacomcontractor.com>
2024-04-16 08:17:55 -0400

    Implementation of error handling.

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,28 +1,12 @@
-import { db } from "@/db/db";
-import {  redirect } from "next/navigation";
+"use client";
+import { useFormState } from "react-dom";
+import * as actions from "@/app/actions";
 const CreateSnippetPage = () => {
-  const createSnippet = async (formData: FormData) => {
-    // This is a server action
-    "use server";
-
-    // Check for valid input
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    // Create a new record in DB
-
-    const snippet = await db.snippet.create({
-      data: { title, code },
-    });
-
-    console.log(snippet);
-
-    // Redirect to the home page
-    redirect("/");
-  };
-
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: "",
+  });
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -49,6 +33,12 @@ const CreateSnippetPage = () => {
             id="code"
           />
         </div>
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+            {" "}
+            {formState.message}{" "}
+          </div>
+        ) : null}
         <button type="submit" className="border rounded p-2 bg-blue-200">
           Create
         </button>
